15ca3137f4316923c236aef12fece5ab3cd2ee56
StevenLwcz <stevenlwcz@virginmedia.net>
2024-10-26 16:33:17 +0100

    Hex address display not increasing in 16 each row. Address was a pointer to int so +1 meant +4. Make it an integer.

--- memview.py
+++ memview.py
@@ -35,7 +35,7 @@ Memory view at the address of the expression"""
             print("memview: can't evaluate {arguments}")
             return
 
-        addr = expr.address if expr.address != None else expr
+        addr = int(expr.address) if expr.address != None else expr
 
         if self.win == None: 
             gdb.execute("layout memview")
